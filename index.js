5569c6faa3f5a36f169de1acc27a699f639b3a67
Micah Banschick <micha.ban@gmail.com>
2021-03-06 01:29:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const tutorials = [
   'What is the Contutorialuctor OO pattern?',
   'implementing Blockchain Web API',
   'The Test Driven Development Workflow',
-  'What is NaN and how Can we Check for It',
+  'What is NaN and how Can we Check for it',
   'What is the difference between stopPropagation and preventDefault?',
   'Immutable State and Pure Functions',
   'what is the difference between == and ===?',
