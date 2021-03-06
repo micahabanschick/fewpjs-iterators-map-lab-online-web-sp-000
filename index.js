bb30d76691220b3510fcce322c88f622a7fae539
Micah Banschick <micha.ban@gmail.com>
2021-03-06 01:27:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const tutorials = [
-  { name: 'What does the this keyword mean?', duration: 80 },
-  { name: 'What is the Constructor OO pattern?', duration: 40 },
-  { name: 'Implementing Blockchain Web API', duration: 100 },
-  { name: 'The Test Driven Development Workflow', duration: 140 },
-  { name: 'What is NaN and how can we check for it', duration: 50 },
-  { name: 'What is the difference between stopPropagation and preventDefault?', duration: 90 },
-  { name: 'Immutable State and Pure Functions', duration: 75 },
-  { name: 'What is the difference between == and ===?', duration: 30 },
-  { name: 'What is the difference between event capturing and bubbling?', duration: 60 },
-  { name: 'What is JSONP?', duration: 25 },
+  'what does the this keyword mean?',
+  'What is the Contutorialuctor OO pattern?',
+  'implementing Blockchain Web API',
+  'The Test Driven Development Workflow',
+  'What is NaN and how Can we Check for it',
+  'What is the difference between stopPropagation and preventDefault?',
+  'Immutable State and Pure Functions',
+  'what is the difference between == and ===?',
+  'what is the difference between event capturing and bubbling?',
+  'what is JSONP?'
 ];
 
 const titleCased = () => {
